ab721facd04cdb6c9adb942b44ce4925ec2397b9
mikolaj6r <35143306+mikolaj6r@users.noreply.github.com>
2020-07-02 13:50:38 +0200

    Update build-and-deploy-deno.sh

--- deno/build-and-deploy-deno.sh
+++ deno/build-and-deploy-deno.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+
 denoarr=( $(curl -sL https://api.github.com/repos/denoland/deno/tags | jq --raw-output '.[] | .name | ltrimstr("v")' ))
 dockerarr=( $(curl -sL https://registry.hub.docker.com/v1/repositories/mikolaj6r/deno/tags | jq --raw-output '.[] | .name | ltrimstr("v")' ))
 
